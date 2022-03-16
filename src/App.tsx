d2df72b5a5d6c9fab6620218e55ba121d6df5a52
Vinícius <viniciusc.miranda@outlook.com>
2022-03-16 13:14:25 -0300

    hotfix

--- src/App.tsx
+++ src/App.tsx
@@ -2,35 +2,33 @@ import { Game } from 'components/Game';
 import { DataProvider } from 'contexts/data';
 import { GameProvider } from 'contexts/game';
 import { ToastProvider } from 'contexts/toast';
-import React, { useEffect } from 'react';
+import React from 'react';
 import TagManager from 'react-gtm-module';
 import { global } from 'styles';
 import { useRegisterSW } from 'virtual:pwa-register/react';
 
 export const App: React.FC = () => {
-  useEffect(() => {
-    // force https
-    if (
-      import.meta.env.PROD &&
-      window.location.hostname !== 'localhost' &&
-      window.location?.protocol !== 'https:'
-    ) {
-      window.location.protocol = 'https:';
-    }
+  // initialize pwa
+  useRegisterSW({
+    onRegistered: (r) => r && setInterval(() => r.update(), 60 * 60 * 1000),
+  });
 
-    // initialize tag manager
-    TagManager.initialize({
-      gtmId: 'GTM-WFWB5GG',
-    });
+  // force https
+  if (
+    import.meta.env.PROD &&
+    window.location.hostname !== 'localhost' &&
+    window.location?.protocol !== 'https:'
+  ) {
+    window.location.protocol = 'https:';
+  }
 
-    // initialize pwa
-    useRegisterSW({
-      onRegistered: (r) => r && setInterval(() => r.update(), 60 * 60 * 1000),
-    });
+  // initialize tag manager
+  TagManager.initialize({
+    gtmId: 'GTM-WFWB5GG',
+  });
 
-    // initialize global styles
-    global();
-  }, []);
+  // initialize global styles
+  global();
 
   return (
     <ToastProvider>
