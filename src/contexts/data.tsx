8844656de17106e5822d40d3a3d04c8210e08dfe
Vini <viniciusc.miranda@outlook.com>
2023-03-23 20:49:39 -0300

    remove puzzle match by year

--- src/contexts/data.tsx
+++ src/contexts/data.tsx
@@ -67,13 +67,22 @@ export const DataProvider: React.FC = ({ children }) => {
 
       if (!random) {
         nextPuzzle = puzzles.find((p, pIndex) => {
-          if (moment(p.date).isSame(moment(), 'day')) {
+          const puzzleDate = moment(p.date);
+          const isSameDay = puzzleDate.format('DD') === moment().format('DD');
+          const isSameMonth = puzzleData.format('MM') === moment().format('MM');
+          
+          if (isSameDay && isSameMonth)) {
             nextLastSolution = puzzles[pIndex - 1]?.solution || null;
             return true;
           }
 
           return false;
         });
+        
+        
+        const id = nextPuzzle.id;
+        const daysDiff = moment().diff(moment(nextPuzzle.date), 'days')
+        nextPuzle.id = id + daysDiff
       }
 
       if (random || !nextPuzzle) {
